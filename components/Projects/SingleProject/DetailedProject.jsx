37e1ab9a77049e65f125e887fd2f3ce60b3cd388
Olivier Paspuel <olivier_paspuel@hotmail.com>
2023-04-08 16:00:12 -0500

    feat: Header including title and logo completed

--- components/Projects/SingleProject/DetailedProject.jsx
+++ components/Projects/SingleProject/DetailedProject.jsx
@@ -4,12 +4,12 @@ import Link from 'next/link';
 import { useSession } from 'next-auth/react';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Typography from '@mui/material/Typography';
+import Image from 'next/image';
 
 import { getSingularProject } from 'services/projects';
 import StyledDetailedProject from './StyledDetailedProject';
 
 const DetailedProject = () => {
-
   const { data: session } = useSession();
 
   const router = useRouter();
@@ -21,23 +21,31 @@ const DetailedProject = () => {
   const projectUrl = `${router.basePath}/organizations/${session?.user?.organization}/projects`;
 
   useEffect(() => {
-    getSingularProject(projectId).then(res => {setProject(res.data);});
-
+    if(projectId){
+      getSingularProject(projectId).then((res) => {
+        setProject(res.data);
+      });
+    }
   }, [projectId]);
+  console.log(project);
 
   return (
     <StyledDetailedProject>
       <Breadcrumbs aria-label="breadcrumb">
-        <Link
-          underline="hover"
-          color="inherit"
-          href={projectUrl}
-        >
-    Projects
+        <Link underline="hover" color="inherit" href={projectUrl}>
+          Projects
         </Link>
         <Typography color="text.primary">Project Details</Typography>
       </Breadcrumbs>
-      <h1>{project.name}</h1>
+      <div className='project-name'>
+        <div className="project-logo">
+          <Image width={30} height={30} src={project.logo} alt="Project Logo" />
+        </div>
+        <h1 className="projectName" >{project.name}</h1>
+      </div>
+      <div className="projectId" >
+        <h3>ID {project.id}</h3>
+      </div>
     </StyledDetailedProject>
   );
 };
--- components/Projects/SingleProject/StyledDetailedProject.js
+++ components/Projects/SingleProject/StyledDetailedProject.js
@@ -1,7 +1,26 @@
 import styled from 'styled-components';
 
 const StyledDetailedProject = styled.section`
-
+    .project-name{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        width:: 100%;
+        .project-logo{
+            margin-right: 0.5em;
+        }
+        .projectName{
+            font-weight: 500;
+        }
+    }
+    .projectId{
+        h3{
+            font-weight: 300;
+            font-size: 1em;
+        }
+        margin-top: -1em;
+        width: fit-content;
+    }
 `;
 
 export default StyledDetailedProject;
